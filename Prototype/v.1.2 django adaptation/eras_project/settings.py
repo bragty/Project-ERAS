b59904cb0d2d1f8850ddf370eb097ab8cdc0ac40
erdemgoe <erdemoge@students.zhaw.ch>
2026-08-18 19:00:21 +0200

    allowing localhost and floating ip

--- Prototype/v.1.2 django adaptation/eras_project/settings.py
+++ Prototype/v.1.2 django adaptation/eras_project/settings.py
@@ -4,7 +4,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-eras-prototype-development-key"
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "160.85.252.95",
+    "127.0.0.1",
+    "localhost",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
